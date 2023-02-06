6dd9f1254f5f8694d535a1d57b5a6592c6b02036
Caleb <73370112+CHemmestad@users.noreply.github.com>
2023-02-06 13:40:29 -0600

    Update AirlineSeatingTester.java

--- AirlineSeatingTester.java
+++ AirlineSeatingTester.java
@@ -6,21 +6,23 @@ public class AirlineSeatingTester {
         Scanner input = new Scanner(System.in);
         Boolean cont = true;
         Boolean quit = false;
+        
+        // Loops the program until the user decides to quit
         do {
             cont = true;
-            System.out.println("Pick an option (Add, Show, Quit) : ");
+            System.out.println("Pick an option (Add, Show, Quit) : "); // Gives you the options to choose from
             String option = input.next();
             switch(option){
-                case "Add" :
+                case "Add" : // Adds a passenger to the airplane
                 while (cont) {
-                    Airplane.addPassenger();
-                    cont = Airplane.quit();
+                    Airplane.addPassenger(); // Algorythm for adding a passenger
+                    cont = Airplane.quit(); // Returns bool for exiting the loop for adding passengers
                 }
                 break;
-                case "Show" :
+                case "Show" : // Shows the current seating of the airplane
                 Airplane.printSeating();
                 break;
-                case "Quit" :
+                case "Quit" : // Quits the program
                 quit = true;
                 break;
                 default :
